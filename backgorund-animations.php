75b46987fcd0423e6d6a6f1282fae58b2493e802
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2020-12-29 23:01:18 +0100

    add final epic and epic firework

--- backgorund-animations.php
+++ backgorund-animations.php
@@ -1,29 +1,23 @@
 <?php
+
 /**
  * Footer.
- * @copyright Copyright (C) 2020 Josef Pepik Kahoun, Filip Fufon Tronicek, Petr Kondor Kucera
+ * @copyright Copyright (C) 2020 Josef Kahoun, Filip Tronicek, Petr Kucera
  * @license GPL(GPLv3)
  */
-?>
-
-<!-- tato animace by se mela vypnout, kdyz je silvestr -->
-<!-- <canvas id="canvas"></canvas> -->
-
-<!-- tato animace by se mela zapnout, kdyz je silvestr -->
-<?php
 
 date_default_timezone_set('Europe/Prague');
 
 $endDate = strtotime("January 1 2021 4AM");
-$diff = ceil($endDate-time());
+$diff = ceil($endDate - time());
 
-if($diff > 0) {
-    echo '
+if ($diff < 43200) {
+  echo '
         <div id="container"></div>
     ';
 } else {
-    echo '
-        <!-- tato animace by se mela vypnout(spise zapnout), kdyz je silvestr -->
+  echo '
+        <!-- tato animace by se mela vypnout, kdyz je silvestr -->
         <canvas id="canvas"></canvas>
     ';
 }
@@ -31,43 +25,42 @@ if($diff > 0) {
 ?>
 
 <style>
-/* body {
+  /* body {
     background-color: black;
 } */
 
-#container {
-  margin: 0 auto;
-  width: 100%;
-  height: 90vh;
-  position: fixed;
-  bottom: 0;
-  z-index: -50;
-  border: none;
-}
-
-@media (prefers-reduced-motion) {
   #container {
-    display: none;
-  } 
-}
+    margin: 0 auto;
+    width: 100%;
+    height: 90vh;
+    position: fixed;
+    bottom: 0;
+    z-index: -50;
+    border: none;
+  }
 
+  @media (prefers-reduced-motion) {
+    #container {
+      display: none;
+    }
+  }
 </style>
 
-<script src="https://cdn.jsdelivr.net/npm/fireworks-canvas@2.5.0/fireworks.js"> </script> 
+<script src="https://cdn.jsdelivr.net/npm/fireworks-canvas@2.5.0/fireworks.min.js"> </script>
 
 <script>
-function ffxinit(Fireworks, container) {
-  const loadedTS = Date.now();
-  const nowWorks = new Fireworks(container, {
-    maxRockets: 20, //max # of rockets to spawn
-    rocketSpawnInterval: 1000, // millisends to check if new rockets should spawn
-    numParticles: 100, // number of particles to spawn when rocket explodes (+0-10)
-  });
-  nowWorks.resetSize();
-  setInterval(() => {
-    if (document.hasFocus()) nowWorks.fire();
-  }, 300);
-}
-console.log("Pepa je pípa a Filip zase Fífa");
-ffxinit(Fireworks, document.getElementById("container"));
+  function ffxinit(Fireworks, container) {
+    const loadedTS = Date.now();
+    const nowWorks = new Fireworks(container, {
+      maxRockets: 20, //max # of rockets to spawn
+      rocketSpawnInterval: 1000, // millisends to check if new rockets should spawn
+      numParticles: 100, // number of particles to spawn when rocket explodes (+0-10)
+    });
+    nowWorks.resetSize();
+    setInterval(() => {
+      if (document.hasFocus()) nowWorks.fire();
+    }, 300);
+  }
+
+  ffxinit(Fireworks, document.getElementById("container"));
 </script>
