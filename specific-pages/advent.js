772526ff00fd914488465f0cb0e488e3d0006cdb
petrkucerak <petr.kucera@mainstream.cz>
2021-11-08 16:00:00 +0100

    Add function getSatusByDate.

--- specific-pages/advent.js
+++ specific-pages/advent.js
@@ -2,10 +2,18 @@ function renderAdventCalendar() {
     // code;
 }
 
+/**
+ * 
+ * @param {} date in format "YYYY-MM-DD"
+ * @returns "pass" or "today" or "future" status
+ */
 function getStatusByDate(date){
     let now = new Date();
-    if(Date.now()>date) return "pass";
-    if(Date.now()<)
+    let date_t = new Date(date);
+
+    if(date_t.getDate() > now.getDate()) return "pass";
+    if(date_t.getDate() == now.getDate()) return "today";
+    if(date_t.getDate() < now.getDate()) return "future";
 }
 
 class Pot{
@@ -13,6 +21,9 @@ class Pot{
         this.height = height;
         this.width = width;
         this.date = date;
-        status = getStatusByDate(date);
+        this.status = getStatusByDate(date);
     }
 }
+
+let pot = new Pot(2, 3, "2021-11-08");
+console.log(pot);
