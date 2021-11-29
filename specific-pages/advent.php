d5e3404cc8e82f820d804c8f63311fb9e9322ee6
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-11-29 23:03:56 +0100

    Changed fonts

--- specific-pages/advent.php
+++ specific-pages/advent.php
@@ -1,5 +1,5 @@
 
-<p class="advent-description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta ullam numquam ad et adipisci ratione aut iste atque corrupti voluptatum, ipsam eos voluptas illum molestias quas explicabo, dolorum perferendis nulla!</p>
+<p class="advent-description">Zrať si s nami cekani na jeziska a vyhraj odmeny ze sveta Minecraftu!</p>
 
 <div class="advent-center">
 <button class="advent-botton">Pravidla a dalsi informace</button>
@@ -18,5 +18,5 @@
    #advent-snow {
       background-image: url("<?php echo get_bloginfo('template_directory');?>/assets/img/background_snowflakes.svg");
    }
-   @font-face {font-family:"Minecraft";src:url("<?php echo get_bloginfo('template_directory');?>/assets/fonts/minecraft/minecraft.eot?") format("eot"),url("<?php echo get_bloginfo('template_directory');?>/assets/fonts/minecraft/minecraft.woff") format("woff"),url("<?php echo get_bloginfo('template_directory');?>/assets/fonts/minecraft/minecraft.ttf") format("truetype"),url("<?php echo get_bloginfo('template_directory');?>/assets/fonts/minecraft/minecraft.svg#Minecraft") format("svg");font-weight:normal;font-style:normal;}
+   @font-face {font-family:"Minecraft";src:url("<?php echo get_bloginfo('template_directory');?>/assets/fonts/minecraft.ttf") format("truetype");font-weight:normal;font-style:normal;}
 </style>
