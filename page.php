1f382136f8871a32cf2afda78a190e5d20a4d28f
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-11-29 21:48:24 +0100

    White h1 ony for calendar

--- page.php
+++ page.php
@@ -23,7 +23,7 @@
         ?>
                
         <main class="main post">
-            <header class="main__header post__header">
+            <header class="main__header post__header <?php if(is_page( 'adventni-kalendar' )) echo "advent-header";?>">
                 <h1><?php the_post(); the_title(); ?></h1>
             </header> 
 
