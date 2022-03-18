218ab7d69c5b9297ba82ef09b73c8dab584b3d10
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2022-03-18 12:07:20 +0100

    Add end tag

--- functions.php
+++ functions.php
@@ -372,3 +372,5 @@ function smartwp_remove_wp_block_library_css(){
     // wp_dequeue_style( 'wc-blocks-style' ); // Remove WooCommerce block CSS
 } 
 add_action( 'wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 100 );
+
+?>
