9fa112b9b1f27b80e857f2d1a7c3d193627e0cd8
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2022-03-18 12:15:17 +0100

    Condition filtering page types

--- functions.php
+++ functions.php
@@ -367,9 +367,11 @@ add_filter('xmlrpc_enabled', '__return_false');
  * @author Petr Kucera
  */
 function smartwp_remove_wp_block_library_css(){
-    wp_dequeue_style( 'wp-block-library' );
-    // wp_dequeue_style( 'wp-block-library-theme' );
-    // wp_dequeue_style( 'wc-blocks-style' ); // Remove WooCommerce block CSS
+    if (!is_single() && !is_page()) {
+        wp_dequeue_style( 'wp-block-library' );
+        // wp_dequeue_style( 'wp-block-library-theme' );
+        // wp_dequeue_style( 'wc-blocks-style' ); // Remove WooCommerce block CSS
+    }
 } 
 add_action( 'wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 100 );
 
