3f5397e52241f1418f0e8cb707c54f22ee40e159
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2022-03-18 12:58:23 +0100

    Diseable Gutenberg removing function

--- functions.php
+++ functions.php
@@ -372,7 +372,8 @@ function smartwp_remove_wp_block_library_css(){
         // wp_dequeue_style( 'wp-block-library-theme' );
         // wp_dequeue_style( 'wc-blocks-style' ); // Remove WooCommerce block CSS
     }
-} 
-add_action( 'wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 100 );
+}
+// Replaced by overwriting html :where(img) class attribute in _global.scss
+// add_action( 'wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 100 );
 
 ?>
