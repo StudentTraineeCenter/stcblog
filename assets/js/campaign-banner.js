540799970ab8c9d798d7a55d07d2ebd7b6bf08e1
Petr Kucera <petr@khome.cz>
2022-09-12 17:42:37 +0200

    Add correct code

--- assets/js/campaign-banner.js
+++ assets/js/campaign-banner.js
@@ -29,7 +29,7 @@ function renderBanner() {
      >
      
        <img
-         src="http://localhost/testsite/wp-content/themes/stcblog/assets/img/kampan-2022.svg"
+         src="https://studuj.digital/wp-content/themes/stcblog/assets/img/kampan-2022.svg"
          class="campaign-tag"
          alt="Campaign tag #STC2023"
        />
@@ -38,7 +38,7 @@ function renderBanner() {
        Přidej&nbsp;se do&nbsp;<strong>14&nbsp;11.</strong> do&nbsp;komunity
        Microsoft&nbsp;STC a&nbsp;jdi své&nbsp;kariéře naproti.
      </p>
-     <a href="TODO" rel="nooperner" target="_blank"
+     <a href="https://prihlaska.studentstc.cz/" rel="nooperner" target="_blank"
        ><button>Přihlásit se <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-right" width="40" height="14" viewBox="-14 0 24 20" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
        <line x1="5" y1="12" x2="19" y2="12" />
@@ -55,30 +55,32 @@ function renderSuperBanner() {
   headerEl.insertAdjacentHTML(
     "afterend",
     `
-   <div class="campaign-banner">
-     <a
-       href="https://www.instagram.com/microsoftstc/"
-       rel="nooperner"
-       target="_blank"
-     >
-       <img
-         src="/assets/img/kampan-2022.svg"
-         class="campaign-tag"
-         alt="Campaign tag #STC2023"
-       />
-     </a>
-     <p>
-       Přidej&nbsp;se do&nbsp;<strong>14&nbsp;11.</strong> do&nbsp;komunity
-       Microsoft&nbsp;STC a&nbsp;jdi své&nbsp;kariéře naproti.
-     </p>
-     <a href="TODO" rel="nooperner" target="_blank"
-       ><button>Přihlásit se <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-right" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
-       <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-       <line x1="5" y1="12" x2="19" y2="12" />
-       <line x1="13" y1="18" x2="19" y2="12" />
-       <line x1="13" y1="6" x2="19" y2="12" />
-       </svg></button></a
-     >
-   </div>`
+    <div class="campaign-banner">
+    <a
+      href="https://www.instagram.com/microsoftstc/"
+      rel="nooperner"
+      target="_blank"
+    >
+    
+      <img
+        src="https://studuj.digital/wp-content/themes/stcblog/assets/img/kampan-2022.svg"
+        class="campaign-tag"
+        alt="Campaign tag #STC2023"
+      />
+    </a>
+    <p>
+      Přidej&nbsp;se do&nbsp;<strong>14&nbsp;11.</strong> do&nbsp;komunity
+      Microsoft&nbsp;STC a&nbsp;jdi své&nbsp;kariéře naproti.
+    </p>
+    <a href="https://prihlaska.studentstc.cz/" rel="nooperner" target="_blank"
+      ><button>Přihlásit se <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-right" width="40" height="14" viewBox="-14 0 24 20" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+      <line x1="5" y1="12" x2="19" y2="12" />
+      <line x1="13" y1="18" x2="19" y2="12" />
+      <line x1="13" y1="6" x2="19" y2="12" />
+    </svg></button></a
+    >
+  </div>
+  `
   );
 }
